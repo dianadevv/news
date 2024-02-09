d365de2131cfa232ab1ffe7fe962f879a5345937
dianadevv <diana17.01.99z@gmail.com>
2024-02-09 12:51:55 +0300

    connect API

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const data = {"sources":[{"id":1,"name":"meduza"},{"id":6,"name":"3dnews"},{"id":7,"name":"nytimes"},{"id":8,"name":"forbes"},{"id":9,"name":"igromania"},{"id":10,"name":"buro237"},{"id":11,"name":"rusvesna"},{"id":12,"name":"7ya"},{"id":14,"name":"aif"},{"id":15,"name":"gazetaru"},{"id":16,"name":"karpov.courses"}],"categories":[{"id":1,"name":"tech"},{"id":2,"name":"sport"},{"id":3,"name":"fashion"},{"id":4,"name":"politics"},{"id":5,"name":"other"},{"id":6,"name":"karpov.courses"}],"items":[{"id":498632,"lang":"ru","date":"Fri Feb 02 2024 08:32:49 GMT+0300 (Moscow Standard Time)","title":"Захарова осудила решение Эквадора отправить старую технику России в США","description":"Такой шаг был сделан под серьезным давлением заинтересованных лиц извне.","image":"https://aif-s3.aif.ru/images/035/708/ee7bf3595bd7bd724eb6d4b55a69222a.png","source_id":14,"category_id":4},{"id":498672,"lang":"ru","date":"Fri Feb 02 2024 08:26:00 GMT+0300 (Moscow Standard Time)","title":"Глава Apple в этом году пообещал сделать важное заявление относительно искусственного интеллекта","description":"Генеральный директор Apple Тим Кук (Tim Cook) и ранее делал на квартальных мероприятиях заявления о важности систем искусственного интеллекта для бизнеса компании, но на минувшем он открыто признался, что в текущем году раскроет подробности работы, ведущейся в данном направлении. Apple в сфере ИИ не хочет отставать от лидеров рынка, как дал понять Кук.","image":"https://3dnews.ru/assets/external/illustrations/2024/02/02/1099709/tim_cook_02.jpg","source_id":6,"category_id":1},{"id":498634,"lang":"ru","date":"Fri Feb 02 2024 08:21:00 GMT+0300 (Moscow Standard Time)","title":"Хидео Кодзима рассказал о загадочной кукле из свежего трейлера Death Stranding 2","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/2/2/cb5f4a14-9816-4263-9c38-60d837344acf.jpg","source_id":9,"category_id":1},{"id":498636,"lang":"ru","date":"Fri Feb 02 2024 08:14:54 GMT+0300 (Moscow Standard Time)","title":"В Киргизии в результате аварии на ТЭЦ пострадали 5 человек","description":"В качестве одной из версий происшествия рассматривается диверсия.","image":"https://aif-s3.aif.ru/images/035/708/ab3e4a8273d1de6e4968a1e38fe83a57.jpg","source_id":14,"category_id":5},{"id":498642,"lang":"ru","date":"Fri Feb 02 2024 08:10:59 GMT+0300 (Moscow Standard Time)","title":"Скоростной режим по новой трассе к Крымскому мосту поднимут до 120 км/ч","description":"Дорога пройдет в обход 11 населенных пунктов.","image":"https://aif-s3.aif.ru/images/035/708/6021bb9994d6c15e7ccd4a617dc58d41.jpg","source_id":14,"category_id":4},{"id":498644,"lang":"ru","date":"Fri Feb 02 2024 08:08:01 GMT+0300 (Moscow Standard Time)","title":"Премьер Киргизии прервал визит в Алма-Аты после взрыва на ТЭЦ в Бишкеке","description":"Акылбек Жапаров не сможет принять участие в заседании ЕМПС из-за аварии на ТЭЦ.","image":"https://aif-s3.aif.ru/images/035/708/9c5fdca8e4308766558b4ffde2e01738.JPG","source_id":14,"category_id":4},{"id":498630,"lang":"ru","date":"Fri Feb 02 2024 08:01:10 GMT+0300 (Moscow Standard Time)","title":"Власти Украины оценили в $1 трлн размер компенсации от России за предполагаемый ущерб","description":"Россия должна выплатить Украине компенсацию в размере более $1 трлн, заявил экономический советник президента Зеленского Олег Устенко. По его оценке, такова сумма прямых и косвенных убытков экономики Украины. Ранее размер средств, необходимых для восстановления страны, власти Украины оценивали в $750 млрд","image":"","source_id":8,"category_id":5},{"id":498682,"lang":"ru","date":"Fri Feb 02 2024 08:00:23 GMT+0300 (Moscow Standard Time)","title":"GS Group: российский рынок планшетов за 2023 год рухнул на 27%","description":"В 2023 году поставки планшетов в Россию сократились на 27% по сравнению с прошлым годом. Практически все лидеры рынка потеряли в продажах с 2022 года, один из явных аутсайдеров – Samsung, его продажи просели на 91%. Об этом ...","image":"http://img.gazeta.ru/files3/450/16685450/upload-photo_2023-05-10_21.59-pic905-895x505-41285.jpg","source_id":15,"category_id":1},{"id":498633,"lang":"ru","date":"Fri Feb 02 2024 08:00:00 GMT+0300 (Moscow Standard Time)","title":"КНДР встает на лыжи: кто поедет кататься на северокорейский курорт «Масикрён» ","description":"С начала 2024 года российские и мировые СМИ активно обсуждают северокорейский горнолыжный курорт Масикрён. Поводом тому стало заявление правительства Приморского края о готовности открыть организованные туры на данный объект, а также внезапный интерес к нему «загадочных блогеров». В преимуществах, недостатках и перспективах горнолыжного комплекса в Северной Корее разбирался Forbes","image":"","source_id":8,"category_id":5},{"id":498646,"lang":"ru","date":"Fri Feb 02 2024 07:55:08 GMT+0300 (Moscow Standard Time)","title":"Минимум два человека погибли из-за взрыва и пожара на автозаправке в Кении","description":"Кенийская организация Красного Креста сообщила о 298 пострадавших в результате инцидента.","image":"https://aif-s3.aif.ru/images/035/708/95c55208d45a0e1515f20d600ceabf43.png","source_id":14,"category_id":5},{"id":498669,"lang":"ru","date":"Fri Feb 02 2024 07:51:23 GMT+0300 (Moscow Standard Time)","title":"Курс евро превысил 98 рублей","description":"Курс евро поднялся выше отметки в 98 рублей во время торгов на Мосбирже утром 2 февраля. Как пишет ТАСС, такого показателя евро достиг впервые с 16 января текущего года.\n\nОтмечается, что в 07:00 мск курс евро достиг 98,45 рубля (+0,51 ...","image":"https://img.gazeta.ru/files3/96/17237096/RIA_8128302-pic905-895x505-14554.jpg","source_id":15,"category_id":4},{"id":498648,"lang":"ru","date":"Fri Feb 02 2024 07:46:43 GMT+0300 (Moscow Standard Time)","title":"В Иркутске 9 человек госпитализированы с пищевым отравлением","description":"Все они покупали еду в одной из точек общепита.","image":"https://aif-s3.aif.ru/images/035/708/d0976b49a5a6787e75e8a492ad3d0bd2.JPG","source_id":14,"category_id":5},{"id":498639,"lang":"ru","date":"Fri Feb 02 2024 07:45:02 GMT+0300 (Moscow Standard Time)","title":"Obsidian объяснила ограниченный выбор расы главного героя Avowed","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/2/2/aefba250-11ae-4a5e-b887-4f1d46181779.jpg","source_id":9,"category_id":1},{"id":498651,"lang":"ru","date":"Fri Feb 02 2024 07:42:28 GMT+0300 (Moscow Standard Time)","title":"США вводят санкции против еврейских поселенцев на Западном берегу Иордана","description":"Глава Белого дома Джо Байден подпишет соответствующий указ.","image":"https://aif-s3.aif.ru/images/035/708/eb0d7fdc942c3119353e19657cc6e88d.jpg","source_id":14,"category_id":4},{"id":498661,"lang":"ru","date":"Fri Feb 02 2024 07:42:00 GMT+0300 (Moscow Standard Time)","title":"Убытки Reality Labs достигли исторического максимума в IV квартале 2023 года на фоне конкуренции с Apple","description":"В преддверии старта продаж Apple Vision Pro подразделение Reality Labs компании M**a понесло рекордные убытки в размере $4,65 млрд. Эта цифра превысила прогнозы аналитиков, ожидавших убыток на уровне $4,26 млрд. За всё время с конца 2020 года общие потери этого подразделения составили свыше $42 млрд, а IV квартал стал для Reality Labs самым убыточным.","image":"https://3dnews.ru/assets/external/illustrations/2024/02/02/1099704/ubitki-reality-labs-dostigli-istoricheskogo-maksimuma-v-iv-kvartale-2023-goda-na-fone-konkurentsii-s-apple-main.jpg","source_id":6,"category_id":1},{"id":498647,"lang":"ru","date":"Fri Feb 02 2024 07:41:09 GMT+0300 (Moscow Standard Time)","title":"Путин пообещал продлить программу развития кампусов в России","description":"Президент России Владимир Путин заявил, что программа развития кампусов в России будет продлена.  Об этом сообщает издание \"Ведомости\".\n\nВ ближайшей перспективе программа затронет 17 объектов, а в дальнейшем будет расширена. ...","image":"https://img.gazeta.ru/files3/187/16206187/RIA_8363472-pic905-895x505-77977.jpg","source_id":15,"category_id":4},{"id":498650,"lang":"ru","date":"Fri Feb 02 2024 07:39:53 GMT+0300 (Moscow Standard Time)","title":"В Херсонской области заявили, что переговоры с Украиной должны быть на условиях России","description":"Мирные переговоры между Россией и Украиной для урегулирования конфликта возможны только на условиях Москвы. Об этом заявил губернатор Херсонской области Владимир Сальдо, передает РИА Новости.\n\nПо его словам, переговоры всегда возможны, но ...","image":"https://img.gazeta.ru/files3/205/17664205/5-pic905-895x505-4391.jpg","source_id":15,"category_id":4},{"id":498653,"lang":"ru","date":"Fri Feb 02 2024 07:36:32 GMT+0300 (Moscow Standard Time)","title":"ВС РФ ликвидировали двух грузинских наемников, воевавших за ВСУ с 2014 года","description":"Зура Циклаури и Гоча Жвания состояли в рядах грузинского легиона ВСУ.","image":"https://aif-s3.aif.ru/images/035/708/b76dfdfc5f8bf36f7691aaea3060abd9.JPG","source_id":14,"category_id":5},{"id":498663,"lang":"ru","date":"Fri Feb 02 2024 07:26:00 GMT+0300 (Moscow Standard Time)","title":"Итоги квартала Apple оказались лучше ожиданий, но в Китае выручка сократилась на 13 %","description":"Впервые за четыре предыдущих квартала компании Apple удалось в прошлом отчётном периоде увеличить выручку, пусть и на скромные 2 % до $119,58 млрд. Данный результат оказался лучше ожиданий рынка, и в целом прогнозы аналитиков не оправдались только в сегменте iPad и услуг, но снижение выручки в Китае на 13 % насторожило инвесторов, и акции Apple после закрытия торгов упали в цене почти на 3 %.","image":"https://3dnews.ru/assets/external/illustrations/2024/02/02/1099706/Tim_Cook.jpg","source_id":6,"category_id":1},{"id":498652,"lang":"ru","date":"Fri Feb 02 2024 07:24:43 GMT+0300 (Moscow Standard Time)","title":"В МИД РФ назвали опрометчивым решение Эквадора передать США российскую военную технику","description":"Решение Эквадора передать США российскую военную технику опрометчиво и принято под серьезным давлением заинтересованных лиц извне. Такое мнение высказала официальный представитель министерства иностранных дел России Мария Захарова в ...","image":"https://img.gazeta.ru/files3/119/17987119/56-pic905-895x505-55034.jpg","source_id":15,"category_id":4}]};
-
-{/* <article class="main-article">
-<div class="main-article__image-container">
-  <img src="./images/marra-m7fT6OreZfI-unsplash-2.jpg" alt="@" class="main-article__image">
-</div>
-<div class="main-article__content">
-  <span class="article-category main-article__category">Технологии</span>
-  <h2 class="main-article__title">Отец жанра. Как уже забытый трип-хоп определяет самую популяр…</h2>
-  <p class="main-article__text">Новая мода на топовые наряды необычных цветов. В сезоне – топики, шорты-боксеры, сланцы и сандалии. А также большие солнечные очки и яркая шляпка.</p>
-  <span class="main-article__source article-source">Источник</span>
-</div>
-</article> */}
-
+let data = null; 
 const mainNewsContainer = document.querySelector('.articles__big-culumn');
 const smollNewsContainer = document.querySelector('.articles__small-culumn');
-
-// вырезаю первые 3 новости
-const mainNews = data.items.slice(0,3);
-const smollNews = data.items.slice(3);
 
 const escapeString = ( str ) => {
     const simbols = {
@@ -29,48 +12,57 @@ const escapeString = ( str ) => {
     return str.replace(/[><&]/g, (tag) => {
         return simbols[tag] || tag;
     })
-}
+};
 
-mainNews.forEach((item) => {
-    const template = document.createElement('template');
-    const sourseData = data.sources.find(sourceItem => sourceItem.id === item.source_id);
-    const categorieData = data.categories.find(categorieItem => categorieItem.id === item.category_id);
-
-    template.innerHTML = `
+const createMainNewsItem = (item) => {
+    return `
     <article class="main-article">
         <div class="main-article__image-container">
             <img src="${encodeURI(item.image)}" alt="@" class="main-article__image">
         </div>
         <div class="main-article__content">
-            <span class="article-category main-article__category">${escapeString(categorieData.name)}</span>
+            <span class="article-category main-article__category">${escapeString(data.categories.find(categoryItem => categoryItem.id === item.category_id).name)}</span>
             <h2 class="main-article__title">${escapeString(item.title)}</h2>
             <p class="main-article__text">${escapeString(item.description)}</p>
-            <span class="main-article__source article-source">${escapeString(sourseData.name)}</span>
+            <span class="main-article__source article-source">${escapeString(data.sources.find(sourceItem => sourceItem.id === item.source_id).name)}</span>
         </div>
     </article>
     `
-    mainNewsContainer.appendChild(template.content); 
-});
+}
 
-
-smollNews.forEach((item) => {
-    const template = document.createElement('template');
-    const sourseData = data.sources.find(sourceItem => sourceItem.id === item.source_id);
-    const dateData = new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric'});
-
-    template.innerHTML = `
+const createSmollNewsItem = (item) => {
+    return `
     <article class="smoll-article">
         <h2 class="smoll-article__title">${escapeString(item.title)}</h2>
         <p class="smoll-article__caption">
-            <span class="smoll-article__date article-date">${escapeString(dateData)}</span>
-            <span class="smoll-article__source article-source">${escapeString(sourseData.name)}</span>
+            <span class="smoll-article__date article-date">${escapeString(new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric'}))}</span>
+            <span class="smoll-article__source article-source">${escapeString(data.sources.find(sourceItem => sourceItem.id === item.source_id).name)}</span>
         </p>
     </article>
     `
-    smollNewsContainer.appendChild(template.content);
-});
+}
 
+const renderNews = (categoryId) => {
+    fetch('http://frontend.karpovcourses.net/api/v2/ru/news/' + (categoryId ? categoryId : ''))
+        .then(response => response.json())
+        .then( (responseData) => {
+            data = responseData;
 
+            const mainNews = data.items.slice(0,3);
+            const smollNews = data.items.slice(3);
 
+            mainNews.forEach((item) => {
+                const template = document.createElement('template');
+                template.innerHTML = createMainNewsItem(item);
+                mainNewsContainer.appendChild(template.content); 
+            });
+
+            smollNews.forEach((item) => {
+                const template = document.createElement('template');
+                template.innerHTML = createSmollNewsItem(item)
+                smollNewsContainer.appendChild(template.content);
+            });
+        })
+}
 
 
